f747d2691f9aeb2b141844d741e6a23c4e2c7786
Kevin Robinson <kevinr.zan@gmail.com>
2017-11-20 14:29:02 -0800

    what we have so far

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class ImagerProfile(models.Model):
+    user = models.ForeignKey(User, unique=True)
+    website = models.CharField(('Website'), blank=True)
+    location = models.CharField(('Location'), blank=True)
+    fee = models
+    # website - str | url
+    # location - str
+    # fee - float
+    # camera - str | choices
+    # services - str | multiple choices
+    # bio - str | large text
+    # phone - str
+    # photo_styles - str | multiple choices
+    # user
+    # is_active
