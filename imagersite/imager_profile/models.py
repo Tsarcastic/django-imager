deff06f1e7da2c5f6d0d582ed2c6ac64844c4994
Brendan Davis <brendanmd@gmail.com>
2017-11-21 15:23:21 -0800

    day 1 is mostly done except we still need to set up tests.

--- imagersite/imager_profile/models.py
+++ imagersite/imager_profile/models.py
@@ -17,21 +17,16 @@ STYLEZ = (
 
 class ProfileManager(models.Manager):
     def get_queryset(self):
-        return super(ProfileManager, self).get_queryset()
-        .filter(user.is_active=True)
+        return super(ProfileManager, self).get_queryset().filter(user__is_active=True)
 
 
 class ImagerProfile(models.Model):
     """Profile for users of the site.
     Connects with Django's built-in User model"""
 
-    def is_active(self):
-        return user.is_active
+    active = ProfileManager()
 
-    active = models.ProfileManager()
-    profiles = models.Manager()
-
-    user = models.ForeignKey(User, unique=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, unique=True)
     website = models.URLField(blank=True)
     location = models.CharField(max_length=255, blank=True)
     fee = models.FloatField(blank=True)
@@ -40,3 +35,27 @@ class ImagerProfile(models.Model):
     bio = models.TextField(blank=True)
     phone = models.CharField(blank=True, max_length=255)
     photo_styles = models.CharField(max_length=255, choices=STYLEZ)
+
+    @property
+    def is_active(self):
+        return self.user.is_active
+
+    def __repr__(self):
+        return """
+        user: {}
+        website: {}
+        location: {}
+        fee: {}
+        camera: {}
+        services: {}
+        bio: {}
+        phone: {}
+        photo_styles: {}""".format(self.user,
+                                   self.website,
+                                   self.location,
+                                   self.fee,
+                                   self.camera,
+                                   self.services,
+                                   self.bio,
+                                   self.phone,
+                                   self.photo_styles)
--- imagersite/imager_profile/tests.py
+++ imagersite/imager_profile/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+"""Test for first day on imager site."""
 
+from django.test import TestCase
+from imager_profile.models import ImagerProfile, User   
+import pytest
+import factory_boy
 # Create your tests here.
+
+
+class ProfileTestCase(TestCase):
+    """Test test."""
+
+    def test_add_user(self):
+        """Test adding a user."""
+        user = User(username='nemo', email='nemo@none.net')
+        user.set_password = "blahblahblah"
+        user.save
+        profile = ImagerProfile(website='www.nope.net',
+                                location='nowhere',
+                                fee=1000000.55,
+                                bio='Herp de derp',
+                                phone='555-5555',
+                                user=user)
+        profile.save()
+
+    def test_user_has_website(self):
+        """Test that a user has a website."""
+        
